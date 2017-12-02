dd2f2b1a271fe442345338b5a17b49d9a2992b74
francesco <francescodaghero@gmail.com>
2017-12-02 17:25:09 +0100

    Ho aggiunto le varie eccezioni, il timer e creato la classe Input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,56 @@
 package com.company;
+import java.util.*;
+
 
 public class Main {
 
+
+
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        //questo metodo fa schifo , non so se fermi i vari thread
+        try {
+
+            if (args.length < 3) {
+                throw new InvalidArgumentNumber();
+            }
+                if (!args[1].contentEquals("-t")) {
+                throw new InsertTime();
+
+            }
+            //Timer setting
+            Timer timer = new Timer();
+            long time = Long.valueOf(args[2]) * 1000; //To have the timer in seconds
+            if (time <= 0) {
+                throw new TimeTooLow();
+            }
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    System.exit(0);
+                }
+            }, time);
+
+
+            while (true) ;
+        } catch (NumberFormatException nfe) {
+            System.out.println("The third argument must be a number");
+            System.exit(1);
+
+        } catch (InvalidArgumentNumber argumentNumber) {
+            System.out.println("Not enough arguments");
+            System.exit(1);
+        } catch (TimeTooLow timeTooLow) {
+            System.out.println("Increase time value");
+            System.exit(1);
+        } catch (InsertTime insertTime) {
+            System.out.println("Tip:Try again with -t");
+        }
+
     }
 }
+class TimeTooLow extends Exception { }
+
+class InvalidArgumentNumber extends Exception { }
+
+class InsertTime extends Exception {}
