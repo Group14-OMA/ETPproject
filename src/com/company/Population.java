ed1043ee6aa0711c60c61de478d3b5814be8bbd2
MaizeDaniele <danielecaschili@tiscali.it>
2017-12-11 19:15:31 +0100

    Solved BestChromosome Selection

--- src/com/company/Population.java
+++ src/com/company/Population.java
@@ -8,26 +8,36 @@ public class Population {
 
     private double bestChromosome;                       //BEST OBJ FUNCTION VALUE IN THE ACTUAL POPULATION
     private ArrayList<Chromosome> populationList;
+    private Integer timeslots;
+    private Integer studentNumber;
+    private Integer examNumber;
+    private Integer[][] conflictMatrix;
+
 
     //CONSTRUCTOR
-    public Population(){
-        bestChromosome = 0;
+    public Population(Integer timeSlots, Integer studentNumber, Integer examNumber, Integer[][] conflictMatrix) {
         populationList = new ArrayList<>();
+        this.timeslots = timeSlots;
+        this.studentNumber = studentNumber;
+        this.examNumber = examNumber;
+        this.conflictMatrix = conflictMatrix;
+        bestChromosome = Math.pow(2, 5) * this.studentNumber;       //Worst case. For sure a normal feasible solution will have a lower value.
     }
-
 
 
     public double getBestObjectiveFunc() {
         return bestChromosome;
     }
 
-    public Chromosome getChromosome(Integer n){
+    public Chromosome getChromosome(Integer n) {
         return populationList.get(n);
     }
 
-    public void removeChromosome(Integer n){ this.populationList.remove(n); }
+    public void removeChromosome(Integer n) {
+        this.populationList.remove(n);
+    }
 
-    public void addChromosome(Chromosome chromosomeToAdd){
+    public void addChromosome(Chromosome chromosomeToAdd) {
         this.populationList.add(chromosomeToAdd);
     }
 
@@ -37,9 +47,10 @@ public class Population {
 
     //Set best value for objective function in the actual population
     public void setBestChromosome() {
-        for(Chromosome c : populationList){
-            if(c.getObjFunc() > this.bestChromosome){ //TODO: c.getObjFunc() < this.bestChromosome, and I need to make sure that at the 1st cycle, bestChromosome is updated (otherwise it will stay at 0)
+        for (Chromosome c : populationList) {
+            if (c.getObjFunc() < this.bestChromosome) {
                 this.bestChromosome = c.getObjFunc();
+
             }
         }
     }
